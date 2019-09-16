54bca30f50dff7d90a5e92b111743daf8fb051af
Jacob Archambault <jacob.archambault@waystar.com>
2019-09-16 09:35:36 -0400

    Replace anonymous method syntax with Lambda expression syntax.

--- Program.cs
+++ Program.cs
@@ -8,19 +8,18 @@ namespace SimpleLambdaExpressions
         static void Main(string[] args)
         {
             Console.WriteLine("***** Fun with Lambdas *****\n");
-            AnonymousMethodSyntax();
+            LambdaExpressionSyntax();
             Console.ReadLine();
         }
 
-        static void AnonymousMethodSyntax()
+        static void LambdaExpressionSyntax()
         {
             // Make a list of integers.
             List<int> list = new List<int>();
             list.AddRange(new int[] { 20, 1, 4, 8, 9, 44 });
 
             // Call FindAll() using traditional delegate syntax.
-            List<int> evenNumbers = list.FindAll(delegate (int i)
-            { return (i % 2) == 0; });
+            List<int> evenNumbers = list.FindAll(i => (i % 2) == 0);
 
             Console.WriteLine("Here are your even numbers:");
             foreach (int evenNumber in evenNumbers)
