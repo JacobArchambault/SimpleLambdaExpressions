a12fd99302fca3f3157d475563c72155ba8bb0ac
Jacob Archambault <jacob.archambault@waystar.com>
2019-09-16 09:32:09 -0400

    Replace traditional delegate syntax with anonymous method syntax.

--- Program.cs
+++ Program.cs
@@ -8,19 +8,19 @@ namespace SimpleLambdaExpressions
         static void Main(string[] args)
         {
             Console.WriteLine("***** Fun with Lambdas *****\n");
-            TraditionalDelegateSyntax();
+            AnonymousMethodSyntax();
             Console.ReadLine();
         }
 
-        static void TraditionalDelegateSyntax()
+        static void AnonymousMethodSyntax()
         {
             // Make a list of integers.
             List<int> list = new List<int>();
             list.AddRange(new int[] { 20, 1, 4, 8, 9, 44 });
 
             // Call FindAll() using traditional delegate syntax.
-            Predicate<int> callback = IsEvenNumber;
-            List<int> evenNumbers = list.FindAll(callback);
+            List<int> evenNumbers = list.FindAll(delegate (int i)
+            { return (i % 2) == 0; });
 
             Console.WriteLine("Here are your even numbers:");
             foreach (int evenNumber in evenNumbers)
@@ -28,12 +28,6 @@ namespace SimpleLambdaExpressions
                 Console.Write( "{0}\t", evenNumber);
             }
             Console.WriteLine();
-        }
-
-        static bool IsEvenNumber(int i)
-        {
-            // Is it an even number?
-            return (i % 2) == 0;
         }
     }
 }
